5d725d55acbb89812292b53ca60ebd7b87b8ac08
Angelina <unliantazy@gmail.com>
2021-05-27 21:25:29 +0300

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Redirect, Route, Switch} from 'react-router-dom';
+import './App.css';
+import {Profile} from "./components/Profile";
+import {Login} from "./components/Login";
+import {Registration} from "./components/Registration";
+import {Test} from "./components/Test";
+import {NotFound} from "./components/NotFound";
+
+const App = () => {
+    return (
+        <div className="App">
+            <Switch>
+                <Route exact path={"/"} render={() => <Profile/>}/>
+                <Route path={"/login"} render={() => <Login/>}/>
+                <Route path={"/register"} render={() => <Registration/>}/>
+                <Route path={"/test"} render={() => <Test />}/>
+                <Route path={'*'} render={() => <NotFound/>}/>
+            </Switch>
+        </div>
+    );
+}
+
+export default App;
